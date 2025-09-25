9cf226cda0c8fb67196584d6bc8f2102efd5d40a
nnhattruyen <nntruyen23@gmail.com>
2025-09-25 18:36:53 +0700

    chore: login css

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import { Link, OutlinedInput, InputAdornment , Box, Button, Checkbox, Container, FormControlLabel, FormLabel, Paper, TextField , FormControl, IconButton, Grid } from '@mui/material';
+import { Link, OutlinedInput, InputAdornment , Box, Button, Checkbox, Container, FormControlLabel, FormLabel, Paper, TextField , FormControl, IconButton, Grid, Typography } from '@mui/material';
 import { useState } from 'react';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 
@@ -31,50 +31,148 @@ const Login = () => {
         window.location.href = "/dashboard";
     }
     return (
-        <>
+        <div>
         <Box
+            width={'100%'}
+            height={'100%'}
+            display={'flex'}
+            flexDirection={'row'}
+            top={'0'}
+            left={'0'}
+            position={'fixed'}
+            alignItems={'center'}
             sx={{
-                minHeight: "100vh",
-                backgroundImage: `url(${imageUrl})`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
+                backgroundColor:'rgb(243, 246, 249)',
             }}
         >
-            <Container maxWidth = 'xs'>
-                <Paper elevation={10} sx = {{mt: 8, p: 2}}>
-                    <Box
+            <Box
+                zIndex={10}
+                width={'100%'}
+                height={'100px'}
+                sx={{
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundImage: `url(${'https://gear-viet.vercel.app/static/media/shape.c087bf7aeb7116d08f9ac0693e55b4cb.svg'})`,    
+                    position: 'absolute',
+                    top: '280px',
+                   
+                }}
+            >
+                
+            </Box>
+            <Box
+                height={'380px'}
+                width={'100%'}
+                sx={{
+                    backgroundImage: `url(${imageUrl})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    position: 'absolute',
+                    top: '0px'
+                }}
+            >
+            </Box>
+            
+            
+            <Container 
+                maxWidth = 'xs' 
+                sx={{
+                    zIndex: 40
+                }}
+            >
+                <Paper  
+                    sx = {{ 
+                        p: '24px',
+                        boxShadow: 'rgba(56, 65, 74, 0.15) 0px 1px 2px',
+                    }
+                }>
+                    <Typography 
                         sx={{
-                            color: '#3e744e',
-                            textAlignLast: "center",
-                            fontFamily:  "Roboto, Helvetica, Arial, sans-serif",
+                            m: '0px 0px, 8px',
+                            fontFamily: 'Poppins, sans-serif',
+                            lineHeight: '1.334',
+                            color: 'rgb(64, 81, 137)',
+                            fontSize: '16px',
+                            fontWeight: '600',
+                            textAlign: 'center',
                         }}
                     >
-                        <h1> SIGN IN </h1>
-                    </Box>
+                         Welcome Back !
+                    </Typography>
+                    <Typography
+                        sx={{
+                            fontSize: '.875rem',
+                            lineHeight: '1.25rem',
+                            fontFamily: 'Poppins, sans-serif',
+                            margin: '0px',
+                            fontWeight:'400',
+                            color: 'rgb(135, 138, 153)',
+                            textAlign: 'center',
+                        }}
+                    >
+                         Sign in to continue.
+                    </Typography>
                     <Box
                         sx = {{mt: 1}}
                     >
-                        <FormLabel sx = {{pb: 1}}> UserName </FormLabel>
+                        <FormLabel 
+                            sx = {{
+                                mb: '5rem', 
+                                fontFamily: 'Poppins, sans-serif',
+                                fontSize: '13px', 
+                                fontWeight:'600'
+                            }}
+                        > 
+                            Username 
+                        </FormLabel>
                         <TextField
                             variant="outlined"
                             id='username'
                             fullWidth
                             required
                             autoFocus
-                            sx = {{mb: 2}}
+                            sx = {{
+                                mb: 2, 
+                                mt: 1,
+                                p: 0,
+                                '& .MuiInputBase-root': {
+                                    fontFamily: 'Poppins, sans-serif',
+                                    height: '38px',
+                                    backgroundColor: 'white',
+                                },
+                                '& .MuiOutlinedInput-input': {
+                                    p: '10px',
+                                    fontSize:'13px',
+                                }
+
+                            }}
                             placeholder="Enter username address"
                             value={userName}
                             onChange={(e) => setUserName(e.target.value)}
-                            
                         />
-                        <FormLabel sx = {{pb: 1}}> Password </FormLabel>
+                        <FormLabel 
+                            sx = {{
+                                mb: '5rem', 
+                                fontFamily: 'Poppins, sans-serif',
+                                fontSize: '13px', 
+                                fontWeight:'600'
+                            }}
+                        > 
+                            Password 
+                        </FormLabel>
                         <FormControl
                             variant="outlined"
                             fullWidth
+                            sx={{
+                                mt: 1,
+                                '& .MuiOutlinedInput-input': {
+                                    p: '10px',
+                                    fontSize:'13px',
+                                }
+
+                            }}
                         >   
                             <OutlinedInput
                                 id="password"
@@ -90,10 +188,13 @@ const Login = () => {
                                             onMouseUp={handleMouseUpPassword}
                                             edge="end"
                                         > 
-                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                            <Box sx={{ transform: 'scale(0.7)' }}>
+                                                {showPassword ? <Visibility /> : <VisibilityOff />}
+                                            </Box>
                                         </IconButton>
                                     </InputAdornment>
                                 }
+                                placeholder="Enter password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}                                
                             />
@@ -105,7 +206,16 @@ const Login = () => {
                             <FormControlLabel 
                                 control={<Checkbox value={"remember"} color='primary'/>}
                                 sx={{
-                                    color:"#616161"
+                                    
+                                     
+                                    '& .MuiTypography-root': {
+                                        m: '0',
+                                        fontSize: '14px',
+                                        fontFamily: "Poppins, sans-serif",
+                                        color:"rgb(73, 80, 87)",
+                                        fontWeight: '400',
+                                        lineHeight: '1.5',
+                                    }
                                 }}
                                 label="Remember me"
                             />
@@ -117,7 +227,21 @@ const Login = () => {
                                 textAlignLast: "right"   
                             }}
                         >
-                            <Link href="#" >
+                            <Link href="#" 
+                                className='text-sm  cursor-pointer '
+                                underline="none"
+                                sx={{
+                                    m: '0',
+                                    fontSize: '14.px',
+                                    lineHeight: '1.5',
+                                    fontFamily: "Poppins, sans-serif",
+                                    color:'rgb(135, 138, 153)',
+                                    fontWeight: '400',
+                                    '&:hover': {
+                                        color: '#223767',
+                                    },
+                                }}
+                            >
                                 Forgot Password
                             </Link>
                         </Grid>
@@ -129,31 +253,52 @@ const Login = () => {
                         fullWidth
                         sx={{
                             mt: 1,
-                            backgroundColor: "#3e744e",
+                            backgroundColor: "rgb(10, 179, 156)",
                             mb: 2,
+                            fontFamily: 'Poppins, sans-serif',
+                            fontSize: '13px',
+                            textTransform: 'none',
+                            fontWeight: '600',
                             '&:hover': {
                                 opacity: 1 ,
-                                backgroundColor: "#356443",
-                            }
+                                backgroundColor: "#088f7d",
+                            },
                         }}
                         onClick={handleSubmitSignIn}
                     >
                         Sign In
                     </Button>
-                    <Box
-                        sx={{
-                                alignContent: "center",
-                                textAlignLast: "right"   
+                </Paper> 
+                <Typography
+                    mt={'24px'}
+                    sx={{
+                        textAlignLast: 'center',
+                        fontFamily: 'Poppins, sans-serif',
+                        fontWeight: '400',
+                        lineHeight: '1.5',
+                        fontSize: '14px',
+                    }}
+                   
+                >
+                    Don't have an LoginValuesType ? {" "}
+                    <Typography
+                        component="span"
+                        sx={{ 
+                            color: '#000000', 
+                            fontWeight: '700', 
+                            textDecorationLine: 'underline',
+                            fontSize: '.875rem',
+                            lineHeight: '1.25rem',
+
                         }}
                     >
-                        <Link href="#" >
-                            Resigter
-                        </Link>
-                    </Box>
-                </Paper> 
+                         Sign up
+                    </Typography>
+                </Typography>
             </Container>
+            
         </Box>
-        </>
+        </div>
     );
 }
 export default Login;
