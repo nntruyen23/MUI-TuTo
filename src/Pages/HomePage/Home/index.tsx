9e16a79480ec7e4ec33084bb1f8fa0217da36304
nnhattruyen <nntruyen23@gmail.com>
2025-10-01 10:38:19 +0700

    chore: add css card

--- src/Pages/HomePage/Home/index.tsx
+++ src/Pages/HomePage/Home/index.tsx
@@ -1,27 +1,46 @@
 import { Box, Typography, Card, CardContent } from "@mui/material";
-
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
 const Home = () => {
   const username = 'Anna'
   const cards = [
     {
       title: 'TOTAL EARNINGS',
-      value: '0$',
+      value: '$0',
       profit: '+16.24 %',
+      icon: {
+        bgcolor: '#D3F1ED',
+        i: <MonetizationOnOutlinedIcon sx={{color: '#23BBA6'}} />,
+      },
     },
     {
       title: 'ORDERS',
       value: '0',
       profit: '-3.57 %',
+      icon: {
+        bgcolor: 'rgba(41, 156, 219, 0.18)',
+        i: <ShoppingBagOutlinedIcon sx={{color: 'rgb(41, 156, 219)'}} />,
+      },
     },
     {
       title: 'USERS',
       value: '0',
       profit: '+29.08 %',
+      icon: {
+        bgcolor: '#FEF2DF',
+        i: <AccountCircleOutlinedIcon sx={{color: '#F7B84B'}} />,
+      },
     },
     {
       title: 'NEW PRODUCTION',
       value: '0',
       profit: '+0.00 %',
+      icon: {
+        bgcolor: '#DDE0EA',
+        i: <Inventory2OutlinedIcon sx={{color: '#405189'}} />,
+      },
     },
   ];
 
@@ -40,7 +59,7 @@ const Home = () => {
         >
           <Box
             sx={{
-              
+              mb: '24px',
             }}
           >
             <Typography
@@ -76,42 +95,128 @@ const Home = () => {
                 md: 'repeat(2, 1fr)',  
                 xl: 'repeat(4, 1fr)',  
               },
-              gap: 2,
+              gap: '24px',
             }}
           >
             {cards.map((card, index) => (
-              <Card
-                key={index}
+              <Box
                 sx={{
-                    p: '16px',
-                    borderRadius: '4px',
-                    height: '145px !important',
-                    boxShadow: 'rgba(56, 65, 74, 0.15) 0px 1px 2px',
-                    '&:hover': {
-                      boxShadow: 'rgba(56, 65, 74, 0.15) 0px 4px 8px'
-                    }
+                  height: '145px !important',
                 }}
               >
-                <CardContent>
+                <Card
+                  key={index}
+                  sx={{
+                      borderRadius: '4px',
+                      height: '100%',
+                      boxShadow: 'rgba(56, 65, 74, 0.15) 0px 1px 2px',
+                      '&:hover': {
+                        boxShadow: 'rgba(56, 65, 74, 0.15) 0px 4px 8px'
+                      }
+                  }}
+                >
                   <Box
                     sx={{
-                      fontFamily: 'Poppins, sans-serif',
+                      height: '100%',
+                      width: '100%',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      
                     }}
                   >
-                    <Typography
-                      sx={{
-                        fontSize: '13px'
-                      }}
-                    >
-                      {card.title}
-                    </Typography>
-                    <Typography>
-                      {card.value}
-                    </Typography>           
+                    <CardContent>
+                      <Box
+                        sx={{
+                          width: '100%',
+                          display: 'flex',
+                          flexDirection: 'row',
+                          position: 'relative',
+                        }}
+                      >
+                        <Box>
+                          <Typography
+                            sx={{
+                              fontSize: '13px',
+                              color: '#878A99',
+                              fontWeight: 400,
+                              fontFamily: 'Poppins, sans-serif',
+                            }}
+                          >
+                            {card.title}
+                          </Typography>
+                        </Box>
+                        <Box
+                          sx={{
+                            position: 'absolute',
+                            right:0,
+                          }}
+                        >
+                          <Typography
+                            sx={{
+                                fontFamily: 'Poppins, sans-serif',
+                                fontSize: '13px',
+                                color: 'rgb(10, 179, 156)',
+                                fontWeight: 500,
+                              }}
+                          >
+                            {card.profit}
+                          </Typography> 
+                        </Box>
+                      </Box>
+                      <Box
+                        sx={{
+                          width: '100%',
+                          display: 'flex',
+                          flexDirection: 'row',
+                          mt: '24px',
+                          position: 'relative'
+                        }}
+                      >
+                        <Box>
+                          <Typography
+                            sx={{
+                                fontFamily: 'Poppins, sans-serif',
+                                fontSize: '22px',
+                                color: 'rgb(73, 80, 87)',
+                                fontWeight: 500,
+                                mb: '10px',
+                              }}
+                          >
+                            {card.value}
+                          </Typography> 
+                          <Typography
+                            sx={{
+                                fontFamily: 'Poppins, sans-serif',
+                                fontSize: '13px',
+                                textDecorationLine: 'underline',
+                                color: 'rgb(64, 81, 137)',
+                              }}
+                          >
+                            See details
+                          </Typography> 
+                        </Box>
+                        <Box 
+                          sx={{
+                            position: 'absolute',
+                            right: 0,
+                            bottom: 0,
+                            width: '48px',
+                            height:'48px',
+                            backgroundColor: card.icon.bgcolor,
+                            borderRadius: '4px',
+                            alignContent: 'center',
+                            textAlign: 'center',
+                          }}
+                        >
+                          {card.icon.i}
+                        </Box>
+                      </Box>
+                    </CardContent>
                   </Box>
-                </CardContent>
-                
-              </Card>
+
+                </Card>
+              </Box>
+
             ))}
           </Box>
         </Box>
